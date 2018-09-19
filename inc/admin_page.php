a4810b4bfa923200785f8696e652d85e896aa30b
wayne-dev <tuanphuongdtvn@gmail.com>
2018-09-19 23:38:57 +0700

    admin style

--- inc/admin_page.php
+++ inc/admin_page.php
@@ -1,9 +1,9 @@
 <?php
 add_action('admin_menu', 'wpt_plugin_menu');
 function wpt_plugin_menu() {
-    add_menu_page(__('Table Price Products', 'sadecweb'), __('Table Price', 'sadecweb'), 'manage_options', 'table-prices-product', 'wpt_plugin_options');
+    add_menu_page(__('Pricing Table Products', 'sadecweb'), __('Pricing table ', 'sadecweb'), 'manage_options', 'pricing-table-product', 'wpt_plugin_options');
 
-    add_submenu_page('table-prices-product', __('Settings', 'sadecweb'), __('Settings', 'sadecweb'), 'manage_options', 'sub-page', 'wpt_plugin_settings');
+    add_submenu_page('table-pricing-product', __('Settings', 'sadecweb'), __('Settings', 'sadecweb'), 'manage_options', 'sub-page', 'wpt_plugin_settings');
 }
 
 function wpt_plugin_settings() {
@@ -18,52 +18,41 @@ function wpt_plugin_options() {
     }
     global $wp_roles;
     $roles = $wp_roles->get_names();
-    $cats = array(97, 95, 94);
+    $cats = array(599, 812, 61);
     $i = 0;
     $html_role = '';
     $html_title = '';
     $html_sub_title = '';
+	$roles = array(
+		'trade-only-pricing' => 'Trade ONLY Pricing',
+		'wholesale-pricing' => 'Wholesale Pricing',
+		'retail-pricing' => 'Retail Pricing',
+		);
     foreach($roles as $key => $role) {
         $html_title .= '<td colspan="2" style="text-align: center;"><h4>'.$role.'</h4></td>';
-        $html_sub_title .= '<td style="text-align: center;"><h4>Single</h4></td><td style="text-align: center;"><h4>Double</h4></td>';
+        $html_sub_title .= '<td class="single" style="text-align: center;"><h4>Single</h4></td>
+		<td class="double" style="text-align: center;"><h4>Double</h4></td>';
         $i += 2;
     }
 	?>
-    <style>
-    #table-prices{
-        border-collapse: collapse;
-        margin-bottom: 30px;
-    }
-    #table-prices tr td{
-        border: 1px solid #000;
-    }
-    #button-save{
-        padding: 15px 30px;
-        border: 1px solid #ccc;
-        background: #2196F3;
-        font-size: 16px;
-        border-radius: 5px;
-        cursor: pointer;
-        font-weight: bold;
-        color: #fff;
-    }
-    </style>
-    <h2><?php _e( 'Table Prices', 'sadecweb' ); ?></h2>
-    <table id="table-prices">
+ <link rel='stylesheet' id='thickbox-css'  href='<?php echo WTP_PLUGIN_URL . '/css/price_box.css' ;?>' type='text/css' media='all' />
+   <h2 id = 'table_price_head'><?php _e( 'Pricing Table ', 'sadecweb' ); ?></h2>
+    <table id="table-prices" >
         <tbody>
         <?php 
         foreach($cats as $cat_id) : 
         $term = get_term($cat_id, 'product_cat');
         ?>
-            <tr class="name-cat">
-                <td colspan="<?php echo $i+1; ?>"><h3><?php echo $term->name ?></h3></td>
-            <tr>
+			<tr class="name-cat">
+				<td><h3><?php echo $term->name ?></h3></td>
+				<td colspan="<?php echo $i; ?>"></td>
+			</tr>
             <tr class="name-roles">
-                <td></td>
+                <td  class = "product_name"></td>
                 <?php echo $html_title ; ?>
             </tr>
             <tr class="name-roles-sub">
-                <td></td>
+                <td class="product_name"></td>
                 <?php echo $html_sub_title ; ?>
             </tr>
             <?php
@@ -82,11 +71,11 @@ function wpt_plugin_options() {
                 foreach($products as $product) :
                 ?>
                 <tr class="product" data-id="<?php echo $product->ID ?>">
-                    <td><?php echo get_the_title($product->ID); ?></td>
+                    <td class = "product_name"><?php echo get_the_title($product->ID); ?></td>
                     <?php
                     foreach($roles as $key => $role) {
-                        echo '<td class="single"><input name="_value_single_'.$key.'" id="_value_single_'.$key.'" value="'.get_post_meta($product->ID, '_value_single_'.$key, true).'" size="10"/></td>';
-                        echo '<td class="double"><input name="_value_double_'.$key.'" id="_value_double_'.$key.'" value="'.get_post_meta($product->ID, '_value_double_'.$key, true).'" size="10"/></td>';
+                        echo '<td class="single"><input placeholder = "N/A" name="_value_single_'.$key.'" id="_value_single_'.$key.'" value="'.get_post_meta($product->ID, '_value_single_'.$key, true).'" size="10"/></td>';
+                        echo '<td class="double"><input placeholder = "N/A" name="_value_double_'.$key.'" id="_value_double_'.$key.'" value="'.get_post_meta($product->ID, '_value_double_'.$key, true).'" size="10"/></td>';
                     }
 	                ?>
                 </tr>
